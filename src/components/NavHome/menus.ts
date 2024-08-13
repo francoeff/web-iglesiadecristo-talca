0cda7c86f5d14c6b0451c578c126e37365bac68a
Franco Figueroa Flores <franco.eff95@gmail.com>
2024-08-13 10:37:59 -0400

    feat: add home page

---
+++ src/components/NavHome/menus.ts
@@ -0,0 +1,7 @@
+export const menus = [
+  { icon: 'church', text: 'La iglesia' },
+  { icon: 'bible', text: 'Doctrina' },
+  { icon: 'sermons', text: 'Sermones' },
+  { icon: 'post', text: 'Artículos' },
+  { icon: 'music', text: 'Himnos' },
+];
---
+++ src/components/home/sections/compromises.ts
@@ -0,0 +1,26 @@
+export const compromises = [
+  {
+    icon: 'bible',
+    title: 'Una doctrina bíblica',
+    description:
+      'Respetamos y amamos el evangelio, es por ello que procuramos diligentemente compartir solo la sana doctrina.',
+  },
+  {
+    icon: 'people-group',
+    title: 'La savación de los perdidos',
+    description:
+      'Queremos brindar todo nuestro apoyo para que las personas reciban la salvación por medio del evangelio EN Cristo Jesús.',
+  },
+  {
+    icon: 'church',
+    title: 'La edificación de la iglesia',
+    description:
+      'Queremos servir en la edificación de nuestros hermanos proporcionando contenido que estimule el amor por Dios.',
+  },
+  {
+    icon: 'hands-up',
+    title: 'Glorificar a Dios',
+    description:
+      'Todos nuestros compromisos se sustentan en nuestro único y gran propósito, adorar a nuestro Dios Eterno.',
+  },
+];
